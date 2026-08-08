66ff4c6f3340a7e4edb2c8c3edafb49932627621
a-5amad <ragnarokthor701@gmail.com>
2026-08-08 21:32:49 +0500

    Add Jasmine testing framework

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,5 +1,6 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+console.log("Testing formatCurrency(): ");
 if (formatCurrency(2000.5) === "20.01") {
   console.log("passed!");
 } else {
