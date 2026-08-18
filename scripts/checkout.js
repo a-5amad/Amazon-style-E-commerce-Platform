e6f7c5cc586aa3293d2800bb1c89d7cd71d1c875
a-5amad <ragnarokthor701@gmail.com>
2026-08-18 19:57:28 +0500

    Added backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
+import "../data/backend-practice.js";
+
 //import "../data/cart-class.js";
 
 renderCheckoutHeader();
